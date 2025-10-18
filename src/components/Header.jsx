bb53a774b2f4c513461c4dadde67217e228df1f9
fabinhoeventos <atair.fabinhoeventos@gmail.com>
2025-10-18 14:16:39 -0300

    FIX: ATUALIZA FINAL AS VERSOES DO PROGRAMA 1.2.2 E SERVER

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
-// src/components/Header.jsx (VERSÃO CORRIGIDA COM IMPORTAÇÃO DA LOGO)
+// src/components/Header.jsx (VERSÃO CORRIGIDA PARA USAR A PASTA 'public/')
 
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Link, useLocation } from 'react-router-dom';
 import './Header.css';
 
-// 1. IMPORTE A LOGO A PARTIR DA PASTA 'src/assets'
-import logoSisFO from '../assets/logo2.png'; // <-- AJUSTE O CAMINHO SE NECESSÁRIO
+// GARANTA QUE NÃO HÁ NENHUM 'import logoSisFO ...' AQUI EM CIMA
 
 function Header() {
   const navigate = useNavigate();
@@ -44,8 +43,11 @@ function Header() {
       </div>
       <div className="header-center">
         <Link to="/dashboard" className="header-logo-link" title="Voltar ao Painel de Controle">
-          {/* 2. USE A VARIÁVEL IMPORTADA 'logoSisFO' AQUI */}
-          <img src={logoSisFO} alt="Logo - Voltar ao Início" className="header-logo-img" />
+          {/* AQUI ESTÁ A CORREÇÃO: 
+            Removemos a barra '/' do início do 'src'.
+            Isso torna o caminho relativo e funciona no .exe
+          */}
+          <img src="logo2.png" alt="Logo - Voltar ao Início" className="header-logo-img" />
         </Link>
       </div>
       <div className="header-right">
